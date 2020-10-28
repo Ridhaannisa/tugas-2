e9a96c9044c10956c2e41ed8196046159e2b0922
Ridhaannisa <71063307+Ridhaannisa@users.noreply.github.com>
2020-10-28 21:37:01 +0700

    Initial commit

---
+++ laravel/system/resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link href="assets/css/style.css" rel="stylesheet">
+	  <title>Gp Bootstrap Template - Index</title>
+  <meta content="" name="descriptison">
+</head>
+
+<body>
+	<title>Login LK</title>
+</head>
+<body id="bg-login">
+	<div class="log-1">
+		<h2>Login</h2>
+		<form action="" method="">
+			<input type="text" name="user" placeholder="Username" class="input-control">
+			<input type="password" name="pass" placeholder="Password" class="input-control">
+			<button class="btn"><a href="{{ url("/home")}}">Login</a></button>
+		</form>
+	</div>
+</body>
+</html>
